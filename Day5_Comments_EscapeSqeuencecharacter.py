c98145cd6b9dddccdeb5dc3459a62df7c572f321
Arjun Mehra <you@example.com>
2022-12-25 15:20:40 +0530

    Day_5_comment

---
+++ Day5_Comments_EscapeSqeuencecharacter.py
@@ -0,0 +1,25 @@
+
+# This is single line comment
+
+"""Lorem30 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""" # this is multiline comment
+
+# Here are some examples of escape sequence characters
+
+print("Hello \n World") # /n is new line character
+print("Hello \t World") # \t is tab character
+print("Hello \\ World") # \\ is backslash character
+print("Hello \' World") # \' is single quote character
+print("Hello \" World") # \" is double quote character
+print("Hello \b World") # \b is backspace character
+print("Hello \r World") # \r is carriage return character
+print("Hello \f World") # \f is form feed character
+print("Hello \ooo World") # \ooo is octal value character
+print("Hello \FFF World") # \xhh is hex value character
+# print("Hello \N{name} World") # \N{name} is unicode character
+print("Hello \u1011 World") # \uxxxx is unicode character
+# print("Hello \U10111111World") # \Uxxxxxxxx is unicode character
+# print("Hello \N {U+AFNI054} World") # \N {U+hhhhhhhh} is unicode character
+
+print("Hello \a World") # \a is bell character
+print("Hello \v World") # \v is vertical tab character
+print("Hello \000 World") # \000 is null character
