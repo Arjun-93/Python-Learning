1618d52119d8927b58fb9d0a5fded047517d7875
Arjun Mehra <you@example.com>
2022-12-25 15:08:58 +0530

    Day4

---
+++ Day4_HelloWolrd.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+print("Poem By Arjun")
+
+def write_poem():
+    print("Roses are red")
+    print("Violets are blue")
+    print("I love you")
+    print("And so do you")
+    
+write_poem()
