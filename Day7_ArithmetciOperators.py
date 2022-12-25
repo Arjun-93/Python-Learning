3b5550a84c418ff27a2c0ae7fbbe5812f59fa7a3
Arjun Mehra <you@example.com>
2022-12-25 15:58:35 +0530

    Arithmetic Operators

---
+++ Day7_ArithmetciOperators.py
@@ -0,0 +1,16 @@
+
+# Here are some examples of Arithmetic Operators
+
+print(59 + 3) # Addition
+print(59 - 3) # Subtraction
+print(59 * 3) # Multiplication
+print(59 / 3) # Division
+print(59 % 3) # Modulus
+print(59 ** 3) # Exponentiation
+print(59 // 3) # Floor Division
+print(59 + 3 * 2) # Order of Operations
+print((59 + 3) * 2) # Order of Operations
+print(59 + 3 * 2 ** 2) # Order of Operations
+print((59 + 3) * 2 ** 2) # Order of Operations
+print(59 + 3 * 2 ** 2 // 2) # Order of Operations
+print((59 + 3) * 2 ** 2 // 2) # Order of Operations
