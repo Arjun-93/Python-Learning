1a01d1c3a0ec8fdb22d6d302759df64f2a461fac
Arjun Mehra <you@example.com>
2022-12-25 15:39:19 +0530

    Day_6 Varibales and Datatypes

---
+++ Day6_VariablesandDataTypes.py
@@ -0,0 +1,30 @@
+# Variables and Datatypes 
+
+# Here are some examples of variables and datatypes
+# _int is a container for integer data type
+_int = 1
+_float = 1.0
+_String = "Hello World"
+_char = 'H'
+_Boolean = True
+_complex = 9 + 7j
+# for checking the type of variable use type() function
+print(_int, type(_int))
+print(_float, type(_float))
+print(_String,  type(_String))
+print(_char, type(_char))
+print(_Boolean, type(_Boolean))
+print(_complex, type(_complex))
+
+# in python we can change the type of variable and it is called dynamic typing
+# for example we can change the type of _int variable from integer to string and vice versa 
+# In python programming every variable is an object and every object has a type and a value.
+
+# Some More DataTypes in Python
+List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+Dict = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten"}
+
+print(List, type(List))
+print(Tuple, type(Tuple))
+print(Dict, type(Dict))
